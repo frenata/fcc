38bfc757148294836682be279328a118d229e5e6
Andrew Nichols <danweasel@gmail.com>
2017-03-02 03:02:31 +0700

    Seperate html, css, and js

---
+++ quotes/jquery/app.js
@@ -0,0 +1,35 @@
+// document.ready
+$(() => getQuote());
+
+// retrieve a quote from the API
+function getQuote() {
+	let qodAPI = "http://quotesondesign.com/wp-json/posts?filter" +
+		"[orderby]=rand&filter[posts_per_page]=1&_jsonp=?";
+	$.getJSON( qodAPI, { })
+		.done( response => {
+			let text = response[0].content;
+			let by = response[0].title;
+			setQuote(text,by);
+		});
+}
+
+// write the quote to the page and to the twitter 'share intent'
+function setQuote(text, by){
+	$("#quote").html(text)
+	$("#cite").html("- " + by)
+
+	document.querySelector("#tweet")
+		.setAttribute("href", 
+			"https://twitter.com/share?text=" + 
+			fixedEncodeURIComponent(text.replace(/<\/?p>/g, "")) + 
+			" -" + by);
+}
+
+// from MDN page on EncodeURIComponent
+function fixedEncodeURIComponent(str) {
+	return encodeURIComponent(str).replace(/[!'()*]/g, 
+		c => '%' + c.charCodeAt(0).toString(16));
+}
+
+// pull new quote on button click
+$("#new-quote").click(() => getQuote());
---
+++ wiki/js/app.js
@@ -0,0 +1,64 @@
+// pull from the API on clicking search or pressing enter in input
+document.querySelector("#query").addEventListener("submit", 
+	(e) => {
+		getWiki(document.querySelector("#query input").value);
+		e.preventDefault();
+	}
+	,false);
+
+// open random page when clicking "Random" button
+document.querySelector("#random-btn").addEventListener("click",
+	(e) => {
+		window.open('https://en.wikipedia.org/wiki/Special:Random');
+		e.preventDefault();
+	}
+	,false);
+
+// pull from the wikipedia API
+function getWiki(search) {
+	let url = "https://en.wikipedia.org/w/api.php?" +
+		"action=opensearch&format=json&origin=*&search=" + search;
+	
+	getJSON(url, (err,data) => 
+		err != null ?
+			console.log(err) :
+			writeWikiResults(data[1],data[2],data[3])
+	);
+}
+
+// write the results of the API pull to the page
+function writeWikiResults(title,description,link){
+	clearResults();
+
+	title.forEach((_,i) =>
+		appendResult(title[i],description[i],link[i]));
+}
+
+// remove any old results
+function clearResults() {
+	document.querySelector("#results").innerHTML = "";
+}
+
+// build a div containing a result and add it to the list
+function appendResult(title,description,link) {
+	document.querySelector("#results").innerHTML += 
+		"<a target=\"_blank\" href=\"" 
+		+ link + "\" class=\"result\">" + 
+		"<div>" +
+		"<h3>"+title+"</h3>" +
+		"<p>" + description + "</p>" +
+		"</div></a>";
+}
+
+// pull from a JSON API
+function getJSON(url, callback) {
+	let xhr = new XMLHttpRequest();
+
+	xhr.open("GET", url, true);
+	xhr.responseType = "json";
+	xhr.onload = () => 
+		xhr.status == 200 ?
+			callback(null, xhr.response) :
+			callback(status);
+	xhr.send();
+}
