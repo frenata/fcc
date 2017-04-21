3442f8c26a988abba8bc1fe205b52d40172472dc
Andrew Nichols <danweasel@gmail.com>
2017-04-21 17:15:58 +0700

    Add function comments

--- request-headers/go/src/request-headers/headers.go
+++ request-headers/go/src/request-headers/headers.go
@@ -6,18 +6,21 @@ import (
 	"strings"
 )
 
+// HeadersResponse contains the desired header information
 type HeadersResponse struct {
 	Ip   *string `json:"ipaddress"`
 	Lang *string `json:"language"`
 	Os   *string `json:"software"`
 }
 
+// pretty print the values in JSON format
 func (hr HeadersResponse) String() string {
 	json, _ := json.MarshalIndent(hr, "", "    ")
 	return string(json)
 }
 
-func getHeaders(headers http.Header) string {
+// GetHeaders parses the desired information and returns a JSON object
+func GetHeaders(headers http.Header) string {
 	ip := getIP(headers)
 	lang := getLanguage(headers)
 	os := getOS(headers)
@@ -25,6 +28,7 @@ func getHeaders(headers http.Header) string {
 	return HeadersResponse{ip, lang, os}.String()
 }
 
+// Try getting the IP address from one of several possible headers
 func getIP(headers http.Header) *string {
 	possibleKeys := []string{"Remote_Addr", "Client-IP", "X-Forwarded-For"}
 
@@ -41,6 +45,7 @@ func getIP(headers http.Header) *string {
 	return nil
 }
 
+// Get the Accept-Language header and parse the first locale
 func getLanguage(headers http.Header) *string {
 	language := headers.Get("Accept-Language")
 	if language != "" {
@@ -52,6 +57,7 @@ func getLanguage(headers http.Header) *string {
 	return nil
 }
 
+// Get the User-Agent header and parse the OS information
 func getOS(headers http.Header) *string {
 	os := headers.Get("User-Agent")
 	if os != "" {
--- request-headers/go/src/request-headers/main.go
+++ request-headers/go/src/request-headers/main.go
@@ -7,10 +7,8 @@ import (
 	"os"
 )
 
-// Converts the passed value to a time and writes a JSON response
-// including the natural and unix times.
 func requestHeader(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, getHeaders(r.Header))
+	io.WriteString(w, GetHeaders(r.Header))
 }
 
 func main() {
