0750145c9b282fd45429b0301ab4fe45af688cea
Andrew Nichols <danweasel@gmail.com>
2017-04-21 16:37:53 +0700

    Get ip, language, and user-agent headers, print them to the screen

--- request-headers/go/src/request-headers/main.go
+++ request-headers/go/src/request-headers/main.go
@@ -9,21 +9,56 @@ import (
 
 // Converts the passed value to a time and writes a JSON response
 // including the natural and unix times.
-func headers(w http.ResponseWriter, r *http.Request) {
-	head := r.Header
-	log.Println(head.Get("Remote_Addr"))
+func requestHeader(w http.ResponseWriter, r *http.Request) {
+	headers := r.Header
+	ip := getIP(headers)
+	language := getLanguage(headers)
+	os := getOS(headers)
 
-	for key, value := range head {
+	/*for key, value := range headers {
 		log.Printf("key: %s value: %s", key, value)
+	}*/
+
+	io.WriteString(w, "Request Headers Microservice\n")
+	io.WriteString(w, "Your IP address: "+ip+"\n")
+	io.WriteString(w, "Your language: "+language+"\n")
+	io.WriteString(w, "Your operating system: "+os+"\n")
+}
+
+func getIP(headers http.Header) string {
+	possibleKeys := []string{"Remote_Addr", "Client-IP", "X-Forwarded-For"}
+
+	for _, key := range possibleKeys {
+		if value := headers.Get(key); value != "" {
+			return value
+		}
 	}
 
-	io.WriteString(w, "Request Headers Microservice")
+	return "Not Found"
+}
+
+func getLanguage(headers http.Header) string {
+	language := headers.Get("Accept-Language")
+	if language != "" {
+		return language
+	}
+
+	return "Not Found"
+}
+
+func getOS(headers http.Header) string {
+	os := headers.Get("User-Agent")
+	if os != "" {
+		return os
+	}
+
+	return "Not Found"
 }
 
 func main() {
 	// get bound port of host system
 	port := os.Getenv("PORT")
 
-	http.HandleFunc("/", headers)
+	http.HandleFunc("/", requestHeader)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
